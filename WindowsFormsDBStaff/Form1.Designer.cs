b6ba6eec38818d7074af290fea0e64eb87a2ebba
PetrPetr <ferel2015@yandex.ru>
2019-09-16 11:44:09 +0300

    Add project files.

---
+++ WindowsFormsDBStaff/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace WindowsFormsDBStaff
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("Дерево ");
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.button3 = new System.Windows.Forms.Button();
+            this.dataGridView4 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.DarkKhaki;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 30);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(476, 228);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 264);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(149, 54);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "ADD 1-2 table";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AllowUserToAddRows = false;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(12, 375);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView2.Size = new System.Drawing.Size(487, 150);
+            this.dataGridView2.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(550, 267);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(149, 49);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Вывод из 3 таблиц";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.AllowUserToAddRows = false;
+            this.dataGridView3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView3.BackgroundColor = System.Drawing.Color.DarkGoldenrod;
+            this.dataGridView3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Location = new System.Drawing.Point(550, 30);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView3.Size = new System.Drawing.Size(344, 228);
+            this.dataGridView3.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(238, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Customer";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(238, 343);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Orders";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(722, 343);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(28, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Cars";
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(929, 39);
+            this.treeView1.Name = "treeView1";
+            treeNode1.BackColor = System.Drawing.Color.DarkGoldenrod;
+            treeNode1.Checked = true;
+            treeNode1.Name = "Node0";
+            treeNode1.Text = "Дерево ";
+            this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode1});
+            this.treeView1.Size = new System.Drawing.Size(223, 317);
+            this.treeView1.TabIndex = 8;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(929, 375);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(223, 49);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // dataGridView4
+            // 
+            this.dataGridView4.AllowUserToAddRows = false;
+            this.dataGridView4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView4.BackgroundColor = System.Drawing.Color.Olive;
+            this.dataGridView4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dataGridView4.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView4.Location = new System.Drawing.Point(550, 375);
+            this.dataGridView4.Name = "dataGridView4";
+            this.dataGridView4.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView4.Size = new System.Drawing.Size(344, 150);
+            this.dataGridView4.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1215, 537);
+            this.Controls.Add(this.dataGridView4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridView dataGridView4;
+    }
+}
+
---
+++ WindowsFormsDBStaff/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsDBStaff
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+
+
+        }
+
+
+
+        public async void Button1_Click(object sender, EventArgs e)
+        {
+
+            using (DBStafEntities db = new DBStafEntities())
+            {
+                var Customer = db.Customers.Select(c => new
+                {
+                    Name = c.Name,
+                    Phone = c.Telephone_Number,
+                    ID = c.CustomerId,
+                });
+                dataGridView1.DataSource = Customer.ToList();
+
+                db.SaveChanges();
+            }
+
+            using (DBStafEntities db = new DBStafEntities())
+            {
+                var Order = db.Orders.Select(o => new
+                {
+                    Data = o.OrderDate,
+                    Product = o.Product_Description,
+                    id = o.OrderId,
+                });
+                dataGridView2.DataSource = Order.ToList();
+            }
+
+            using (DBStafEntities db = new DBStafEntities())
+            {
+
+                var Cars = db.Cars.Select(o => new
+                {
+                    Marka = o.Marka,
+                    Owner = o.Owner,
+                    Price = o.Price,
+                    id = o.CarId
+                });
+                dataGridView4.DataSource = Cars.ToList();
+
+
+            }
+
+        }
+
+
+
+
+
+
+//        using (var db = new DBStafEntities())
+//        {
+//            var query = db.Customers.Select(c => new
+//            {
+//                ID_order = c.CustomerId,
+//                c.Name,
+//                Phone = c.Telephone_Number,
+
+
+//            });
+//    dataGridView1.DataSource = query.ToList();
+//         }
+
+//        using (var db = new DBStafEntities())
+//        {
+//            var query = db.Orders.Select(o => new
+//            {
+//                ID_customer = o.OrderId,
+//                Data = o.OrderDate,
+//                Product = o.Product_Description
+//            });
+//dataGridView2.DataSource = query.ToList();
+//        }
+
+
+
+//        using (DBStafEntities db = new DBStafEntities())
+//        {
+//            var Query = db.Customers.Join(db.Orders, customers => customers.CustomerId, orders => orders.OrderId,
+//                ((customers, orders) => new
+//                {
+//                    Name = customers.Name,
+//                    Phone = customers.Telephone_Number,
+
+//                    Data = orders.OrderDate,
+//                    Product = orders.Product_Description
+//                }));
+//dataGridView1.DataSource = Query.ToList();
+//        }
+
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            using (DBStafEntities db = new DBStafEntities())
+            {
+
+                var Query = from customers in db.Customers
+                    join cars in db.Cars on customers.CustomerId equals cars.CarId
+                    join orders in db.Orders on customers.CustomerId equals orders.OrderId
+                    select new
+                    {
+                     Name = customers.Name,
+                     Phone = customers.Telephone_Number,
+                     Id = customers.CustomerId,
+                    };
+
+            }
+        }
+        
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // Ваш код
+        }
+     
+      
+
+
+        //var Query = db.Customers.Join(db.Orders, customers => customers.CustomerId, orders => orders.OrderId,
+        //    (customers, orders) => new
+        //    {
+        //        Name = customers.Name,
+        //        Phone = customers.Telephone_Number,
+        //        Data = orders.OrderDate,
+        //        Product = orders.Product_Description,
+        //        ID = customers.CustomerId,
+        //        id = orders.OrderId,
+
+        //    });
+        //dataGridView3.DataSource = Query.ToArray();
+
+    }
+        }
+
+
+
+        //private void button3_Click(object sender, EventArgs e)
+        //{
+        //    for (int i = 0; i < dataGridView3.Rows.Count; i++)
+        //    {
+        //        TreeNode node = new TreeNode("Данные");
+        //        for (int j = 0; j < dataGridView3.Columns.Count; j++)
+        //        {
+        //            node.Nodes.Add(dataGridView3.Rows[i].Cells[j].Value.ToString());
+        //        }
+
+        //        treeView1.Nodes.Add(node);
+        //    }
+        //}
+
+      
+    
+
+
+
+    
+
